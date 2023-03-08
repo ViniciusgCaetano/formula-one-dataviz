36bb201675988636e4d0b07cce6fe973e1aec7ff
ViniciusGCaetano <viny6888@gmail.com>
2023-03-08 02:02:37 -0300

    add: adding more graphic contents and F1 new data

--- App.py
+++ App.py
@@ -1,3 +1,29 @@
 import streamlit as st
+from Assets.Sidebar import show_sidebar
 
-st.title('Github DataViz')
+
+st.set_page_config(page_title='F1 DataViz', page_icon=None, layout="wide", initial_sidebar_state="expanded", menu_items=None)
+streamlit_style = """
+			<style>
+			@import url('https://fonts.googleapis.com/css2?family=Orbitron:wght@700&display=swap');
+
+			html, body, [class*="css"]  {
+			font-family: 'Orbitron', sans-serif;
+			}
+
+            button {
+                font-family: 'Helvetica', sans-serif !important;
+                width: 100% !important;
+            }
+			</style>
+			"""
+st.markdown(streamlit_style, unsafe_allow_html=True)
+
+st.title('Formula One DataViz')
+st.markdown("""<div style="text-align: justify">Check out my Streamlit App-a-Thon project, which provides fascinating insights into the F1 World Championship. 
+The project offers a range of visualizations that display interesting information about the championship, such as the number of wins by each team, the position of drivers over time, and the 
+distribution of lap times. You can explore the data using various types of charts, including bar charts, line charts, scatter plots, and radar charts. With this project, you'll gain a deeper 
+understanding of the F1 World Championship and its history.</div>""", unsafe_allow_html=True)
+
+show_sidebar()
+
---
+++ Assets/Sidebar.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+
+
+def show_sidebar():
+
+    with st.sidebar:
+
+        st.image("https://th.bing.com/th/id/R.b5420aeaf33a34e10dc5fa6d07874205?rik=zC58R62TClGl6w&pid=ImgRaw&r=0")
+        st.button('Nations')
+        st.button('Compare Drivers')
+        st.button('Circuits')
+        st.button('Seasons')
