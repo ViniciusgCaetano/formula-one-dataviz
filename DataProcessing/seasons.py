fbf0ade59a9ba1de97043861cfcff0e9ee66bd65
ViniciusGCaetano <viny6888@gmail.com>
2023-03-20 02:00:24 -0300

    add: season metrics of races and season winner

--- DataProcessing/seasons.py
+++ DataProcessing/seasons.py
@@ -1,6 +1,30 @@
 import pandas as pd
+from DataProcessing.champions import driver_champions_per_year
 
 def seasons():
     df = pd.read_csv('Data/seasons.csv')
     df.sort_values(by='year', inplace=True, ascending=False)
     return df[1:]
+
+def _total_races(season_year):
+    races = pd.read_csv('Data/races.csv')
+    mask = races['year'] == season_year
+    races = races[mask]
+    total_races = races['round'].max()
+    return total_races 
+
+def _season_driver_winner(season_year):
+    driver_champions =  driver_champions_per_year()
+    mask = driver_champions['year'] == season_year
+    season_champion = driver_champions[mask].iloc[0].surname
+    return season_champion 
+
+
+def metrics(season_year):
+    season_dict = {}
+    season_dict['Total Races'] = _total_races(season_year)
+    season_dict['Champion Driver'] = _season_driver_winner(season_year)
+    return season_dict 
+
+    
+
--- _Pages/Seasons.py
+++ _Pages/Seasons.py
@@ -4,10 +4,17 @@ from Plots.standing_graphs import driver_standing_graph, constructor_standing_gr
 
 def render_page():
     st.title('Seasons')
+
+    
     season_year = st.selectbox('Select a Season', seasons.seasons())
+    metrics = seasons.metrics(season_year)
+    col1, col2, col3 = st.columns(3)
+    col1.metric('Races', metrics['Total Races'])
+    col2.metric('Winner', metrics['Champion Driver'])
     #st.table(standings_per_season.driver_standings_per_season(season_year))
     exp = st.expander('Accumulated Points by Race (Drivers)')
     exp.plotly_chart(driver_standing_graph(season_year), use_container_width=True)
 
     exp = st.expander('Accumulated Points by Race (Constructors)')
     exp.plotly_chart(constructor_standing_graph(season_year), use_container_width=True)
+    
