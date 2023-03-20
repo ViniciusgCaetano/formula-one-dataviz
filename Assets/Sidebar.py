27d8e3b0bcbd59bad17a2356d36138c895191cb1
ViniciusGCaetano <viny6888@gmail.com>
2023-03-20 02:25:25 -0300

    chore: removing buttons from sidebar

--- Assets/Sidebar.py
+++ Assets/Sidebar.py
@@ -11,8 +11,7 @@ def show_sidebar():
         if st.button('Nations', use_container_width=True):
             st.session_state['page'] = 'nations'
             st.experimental_rerun()
-        st.button('Compare Drivers', use_container_width=True)
-        st.button('Circuits', use_container_width=True)
+      
         if st.button('Seasons', use_container_width=True):
             st.session_state['page'] = 'seasons'
             st.experimental_rerun()
