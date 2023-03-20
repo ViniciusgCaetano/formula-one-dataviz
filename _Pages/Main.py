fb49908a3d3b243eca671a2edf0efd938b9a4e44
ViniciusGCaetano <viny6888@gmail.com>
2023-03-20 02:32:21 -0300

    adding more text in home page

--- _Pages/Main.py
+++ _Pages/Main.py
@@ -3,5 +3,5 @@ import streamlit as st
 def render_page():     
     st.title('Formula One DataViz')
     st.markdown("""<div style="text-align: justify">Check out my Streamlit App-a-Thon project, which provides fascinating stats into the F1 World Championship. 
-    The project offers a range of visualizations that display interesting information about the championship, such as the number of wins by each nation, the position of drivers over time
+    The project offers a range of visualizations that display interesting information about the championship, such as the number of wins by each nation and the position of drivers over time
     . With this project, you'll gain a deeper understanding of the F1 World Championship and its history.</div>""", unsafe_allow_html=True)
