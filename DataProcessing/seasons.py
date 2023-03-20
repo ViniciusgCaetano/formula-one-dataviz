918fdc314ef561f318d27d3beb6e657501f428d4
ViniciusGCaetano <viny6888@gmail.com>
2023-03-20 02:06:22 -0300

    add: adding contructor season champion

--- DataProcessing/seasons.py
+++ DataProcessing/seasons.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from DataProcessing.champions import driver_champions_per_year
+from DataProcessing.champions import driver_champions_per_year, constructor_champions_per_year
 
 def seasons():
     df = pd.read_csv('Data/seasons.csv')
@@ -19,11 +19,18 @@ def _season_driver_winner(season_year):
     season_champion = driver_champions[mask].iloc[0].surname
     return season_champion 
 
+def _season_constructor_winner(season_year):
+    constructor_champions =  constructor_champions_per_year()
+    mask = constructor_champions['year'] == season_year
+    season_champion = constructor_champions[mask].iloc[0].RealName
+    return season_champion 
+
 
 def metrics(season_year):
     season_dict = {}
     season_dict['Total Races'] = _total_races(season_year)
     season_dict['Champion Driver'] = _season_driver_winner(season_year)
+    season_dict['Champion Constructor'] = _season_constructor_winner(season_year)
     return season_dict 
 
     
--- _Pages/Seasons.py
+++ _Pages/Seasons.py
@@ -10,7 +10,9 @@ def render_page():
     metrics = seasons.metrics(season_year)
     col1, col2, col3 = st.columns(3)
     col1.metric('Races', metrics['Total Races'])
-    col2.metric('Winner', metrics['Champion Driver'])
+    col2.metric('Winner Driver', metrics['Champion Driver'])
+    col3.metric('Winner Team', metrics['Champion Constructor'])
+
     #st.table(standings_per_season.driver_standings_per_season(season_year))
     exp = st.expander('Accumulated Points by Race (Drivers)')
     exp.plotly_chart(driver_standing_graph(season_year), use_container_width=True)
